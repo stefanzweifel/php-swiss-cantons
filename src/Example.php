f5379cb1aa19875c38bd190d8547437369507982
Stefan Zweifel <hello@stefanzweifel.io>
2016-05-21 16:43:29 +0200

    Init Project
    Signed-off-by: Stefan Zweifel <hello@stefanzweifel.io>

---
+++ src/Example.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Wnx\SwissCantons;
+
+class Example
+{
+
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Wnx\SwissCantons\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+
+}
