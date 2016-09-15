be874f3ca330d58c568e177f87cfc59d7e6c256f
Stefan Zweifel <hello@stefanzweifel.io>
2016-09-15 20:36:40 +0200

    Add Zipcode Search

---
+++ tests/ZipcodeSearchTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Wnx\SwissCantons\Tests;
+
+use Illuminate\Support\Collection;
+use Wnx\SwissCantons\ZipcodeSearch;
+
+class ZipcodeSearchTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_finds_canton_by_zipcode()
+    {
+        $zipcodeSearch = new ZipcodeSearch();
+        $result = $zipcodeSearch->findByZipcode(3005);
+
+        $this->assertInstanceOf(\stdClass::class, $result);
+        $this->assertEquals('BE', $result->canton);
+        $this->assertEquals('Bern', $result->community_name);
+    }
+
+    /** @test */
+    public function it_does_not_find_result_for_not_available_zipcode()
+    {
+        $zipcodeSearch = new ZipcodeSearch();
+        $result = $zipcodeSearch->findByZipcode(99999);
+
+        $this->assertEquals(null, $result);
+    }
+
+    /** @test */
+    public function it_does_not_find_result_if_zipcode_is_passed_as_a_string()
+    {
+        $zipcodeSearch = new ZipcodeSearch();
+        $result = $zipcodeSearch->findByZipcode('FooBar');
+
+        $this->assertEquals(null, $result);
+    }
+}
