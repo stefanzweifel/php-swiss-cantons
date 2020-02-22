c625156bfeff78db851355f4ffe47c78a9fe9a8e
Stefan Zweifel <hello@stefanzweifel.io>
2020-02-22 20:27:34 +0100

    Add custom Exceptions

---
+++ src/Exceptions/InvalidLanguageException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wnx\SwissCantons\Exceptions;
+
+use Exception;
+use Wnx\SwissCantons\Canton;
+
+class InvalidLanguageException extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct('The given language is not supported. Supported languages: '.implode(', ', Canton::AVAILABLE_LANGUAGES));
+    }
+}
