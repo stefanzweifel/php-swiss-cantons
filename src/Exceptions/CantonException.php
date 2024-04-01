ba206f4395607b9a19ff7401a4b370d92c813869
Stefan Zweifel <stefan@stefanzweifel.dev>
2024-04-01 20:14:27 +0200

    Ignore static calls in Exception

--- src/Exceptions/CantonException.php
+++ src/Exceptions/CantonException.php
@@ -8,16 +8,19 @@ class CantonException extends Exception
 {
     public static function notFoundForAbbreviation(string $abbreviation): self
     {
+        /** @phpstan-ignore-next-line */
         return new static("Couldn't find Canton for given abbreviation: {$abbreviation}");
     }
 
     public static function notFoundForName(string $name): self
     {
+        /** @phpstan-ignore-next-line */
         return new static("Couldn't find Canton for given name: {$name}");
     }
 
     public static function notFoundForZipcode(int $zipcode): self
     {
+        /** @phpstan-ignore-next-line */
         return new static("Couldn't find Canton for given zipcode: {$zipcode}");
     }
 }
